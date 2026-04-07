7d2e2c8aacb8ba7059a27bb83cf63eea41b4db1d
Ragavi Gunasekaran <ragavigunasekaran@gmail.com>
2026-04-07 10:32:10 +0200

    added implementation class for subscriptiondao

--- src/main/java/lexicon/workshop/Subscriber.java
+++ src/main/java/lexicon/workshop/Subscriber.java
@@ -7,4 +7,63 @@ public class Subscriber {
     private Plan plan;
     private boolean active;
     private int monthsRemaining;
+
+    public Subscriber(int id, int monthsRemaining, boolean active, Plan plan, String email) {
+        this.id = id;
+        this.monthsRemaining = monthsRemaining;
+        this.active = active;
+        this.plan = plan;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Plan getPlan() {
+        return plan;
+    }
+
+    public void setPlan(Plan plan) {
+        this.plan = plan;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getMonthsRemaining() {
+        return monthsRemaining;
+    }
+
+    public void setMonthsRemaining(int monthsRemaining) {
+        this.monthsRemaining = monthsRemaining;
+    }
+
+    @Override
+    public String toString() {
+        return "Subscriber{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", plan=" + plan +
+                ", active=" + active +
+                ", monthsRemaining=" + monthsRemaining +
+                '}';
+    }
 }
